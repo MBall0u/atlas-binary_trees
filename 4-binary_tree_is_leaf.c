d620f4ab135fe1b96b47663e0c1db3ced849184d
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-08-08 15:29:18 -0500

    M - 3:29 PM - had to take my parent check out of the leaf file

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -7,7 +7,7 @@
 */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-	if (!(node->left) && !(node->right) && node->parent)
+	if (!(node->left) && !(node->right))
 		return (1);
 	return (0);
 }
