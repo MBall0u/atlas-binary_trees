c709fac25137a0de5a10d13ab51a1aeb8b6fd42f
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-08-08 16:42:59 -0500

    M - 4:40 PM - made the file for binary_tree_preorder

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -1,10 +1,10 @@
 #include "binary_trees.h"
 /**
- * 
- * 
- * 
- * 
- * 
+ * binary_tree_preorder - see below
+ * Description - Prints the binary tree in preorder
+ * @tree: the root node passed in
+ * @func: a pointer function passed in
+ * Return: Void
 */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
