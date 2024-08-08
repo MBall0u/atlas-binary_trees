cef3d44d2a713a6368325b92989eb0823f51f52e
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-08-08 15:27:45 -0500

    M - 3:27 PM - made the file for binary_tree_is_leaf

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_is_leaf - see below
+ * Description - checks to see if a node is a leaf or not
+ * @node: the node being checked
+ * Return: Always 1 (success)
+*/
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (!(node->left) && !(node->right) && node->parent)
+		return (1);
+	return (0);
+}
