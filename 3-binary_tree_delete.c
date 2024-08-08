51ba8e7e5fc8959206bdf4c2562f6e32ccc0a55e
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-08-08 17:43:36 +0000

    M - 12:43 PM - made the file binary_tree_delete

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,35 @@
+#include "binary_trees.h"
+/**
+* binary_tree_delete - deletes the binary tree
+* Description - works down the binary tree left to right and
+* frees the node
+* @tree: the root node passed to it
+* Return: Always void (success)
+*/
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree)
+	{
+		if (tree->left == NULL && tree->right == NULL)
+			return;
+		if (tree->left)
+			binary_tree_delete(tree->left);
+		if (tree->right)
+			binary_tree_delete(tree->right);
+		if(tree->left)
+		{
+			free(tree_left);
+			tree->left = NULL;
+		}
+		if (tree->right)
+		{
+			free(tree->right);
+			tree->right = NULL;
+		}
+		if (tree->parent == NULL)
+		{
+			free(tree);
+			tree = NULL;
+		}
+	}
+}
