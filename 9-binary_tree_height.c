8010194e926e51bd21c19e09d7bae99c8636c164
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-08-09 15:17:47 +0000

    M - 10:17 AM - added an un-needed if statement

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -15,13 +15,10 @@ size_t binary_tree_height(const binary_tree_t *tree)
 			left_count = 1 + binary_tree_height(tree->left);
 		if (tree->right)
 			right_count = 1 + binary_tree_height(tree->right);
-		if (!tree->left && !tree->right)
-		{
-			if (left_count >= right_count)
-				return (left_count);
-			if (right_count >= left_count)
-				return (right_count);
-		}
+		if (left_count >= right_count)
+			return (left_count);
+		if (right_count >= left_count)
+			return (right_count);
 	}
 	return (0);
 }
