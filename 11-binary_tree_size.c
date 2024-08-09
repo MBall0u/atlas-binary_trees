e808ede4220d33a5c6bc524d6486e5225f184279
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-08-09 19:11:24 +0000

    M - 2:11 PM - made file for binary_tree_size

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+/**
+* binary_tree_size - see below
+* Description - count from the tree node down
+* @tree: the tree we are counting from
+* Return: Always count (success)
+*/
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t count = 1;
+
+	if (tree)
+	{
+		if(!tree->left && !tree->right)
+			return (1);
+		if (tree->left)
+			count = count + binary_tree_size(tree->left);
+		if (tree->right)
+			count = count + binary_tree_size(tree->right);
+		return (count);
+	}
+	return (0);
+}
