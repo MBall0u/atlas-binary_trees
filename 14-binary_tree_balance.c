31a896d93ef1246faba1a18df1a564e9a523b164
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-08-09 23:24:22 -0500

    M - 11:23 PM - made file for binary_tree_balance

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_balance - see below
+ * Description - finds the balance factor of a tree
+ * @tree: the passed node
+ * Return: Always count (success)
+*/
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	size_t left_count = 0, right_count = 0;
+
+	if (tree)
+	{
+		if (tree->left)
+			left_count = 1 + binary_tree_balance(tree->left);
+		if (tree->right)
+			right_count = 1 + binary_tree_balance(tree->right);
+		return (left_count - right_count);
+	}
+	return (0);
+}
