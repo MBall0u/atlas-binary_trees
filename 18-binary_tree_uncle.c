bb5f153c75ca8cb83e3cb71d343ef1f4d6e3dc71
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-08-10 14:23:51 -0500

    M - 2:23 PM - made file for binary_tree_uncle

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,25 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_uncle - see below
+ * Description - checks node to see if its parent
+ * has a sibling
+ * @node: the passed node to check
+ * Return: uncle node
+*/
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	if (node)
+	{
+		if (node->parent->n == node->parent->parent->right->n)
+		{
+			if (node->parent->parent->left)
+				return (node->parent->parent->left);
+		}
+		if (node->parent->n == node->parent->parent->left->n)
+		{
+			if (node->parent->parent->right)
+				return (node->parent->parent->right);
+		}
+	}
+	return (NULL);
+}
