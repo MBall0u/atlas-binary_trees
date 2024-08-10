4ea8b6e0a6646c4185bde523beb671c5976bc53c
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-08-10 14:16:22 -0500

    M - 2:12 PM - made file for binary_tree_sibling

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -10,6 +10,8 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
 	if (!node || !node->parent)
 		return (NULL);
+	if (!node->parent->right || !node->parent->left)
+		return (NULL);
 	if ((node->n == node->parent->left->n) && node->parent->right)
 		return (node->parent->right);
 	if ((node->n == node->parent->right->n) && node->parent->left)
